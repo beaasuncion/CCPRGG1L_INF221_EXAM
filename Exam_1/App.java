424805b53777bc45da1ad4485dc0ee161e86ff6a
Beaasuncion <111472853+beaasuncion@users.noreply.github.com>
2022-09-05 10:25:36 +0800

    Create App.java

---
+++ Exam_1/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+        // Student class
+        Student Me = new Student ();
+         
+        Me.firstname = "Beatriz";
+        Me.surname = "Asuncion";
+        Me.middlename = "Uy";
+        Me.studentNumber = 2022100598;
+        Me.studentEmailAddress = "asuncionbu@students.national-u.edu.ph";
+        Me.iAmAwesome = true;
+        Me.sayMyStudentNumber();
+        Me.sayMyEmailAddress();
+        Me.iAmAwesome();
+    }
+}   
